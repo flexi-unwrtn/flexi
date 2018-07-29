af99db46a4f9ac4b65c2cb2da20712984f227858
Stephen Kelly <stephen@unwr.tn>
2018-07-29 15:46:51 +1000

    Add files via upload

---
+++ unwrtn_list_mobile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+from itertools import izip
+import csv
+import os.path
+
+html = ''
+csvfile = open('flexi.csv', 'rb')
+reader = csv.reader(csvfile)
+headers = None
+
+for row in reader:
+    if reader.line_num == 1:
+        headers = row
+    else:
+        crow = dict(zip(headers, row))
+        html = html + '<dl><a href="https://d2uzw09ppuvr94.cloudfront.net/title/'+crow['Const']+'" alt="flexi TV-R">'+crow['Title']+'</a><a> ' +' - '+'</a><a href="http://m.imdb.com/title/'+crow['Const']+'" alt="flexi TV-R">'+'IMDb'+'</a></dl>'
+        f = open('flexi.html','w')
+f.write(html)
+f.close()
