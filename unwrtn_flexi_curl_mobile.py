04865ab55281a4380e44446f722814f6aa516f36
Stephen Kelly <stephen@unwr.tn>
2018-08-27 12:52:42 +1000

    Create unwrtn_flexi_curl_mobile.py

---
+++ unwrtn_flexi_curl_mobile.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python2.7
+
+from itertools import izip
+
+import csv
+
+import os.path
+
+
+
+html = ''
+
+csvfile = open('flexi.csv', 'rb')
+
+reader = csv.reader(csvfile)
+
+headers = None
+
+
+
+for row in reader:
+
+    if reader.line_num == 1:
+
+        headers = row
+
+    else:
+
+        crow = dict(zip(headers, row))
+
+        html = html + ':%s+href="/+href="https://www.imdb.com/+g |:/egin TOP_AD/,/nd TOP_AD/d |:/Next/ |:?href?s+https://www.imdb.com++ |:?Previous? |:?href?s+https://www.imdb.com++|:/Episode aired/ |:/    /s+    +\&nbsp;<span class="ghost">|</span>\&nbsp;<a target="_blank" href="https://d2uzw09ppuvr94.cloudfront.net/title/'+crow['Const']+'"><img src="https://raw.githubusercontent.com/Ede123/userscripts/master/icons/Rotten_Tomatoes.png" style="vertical-align: bottom;" width="16" height="16"></a>+ |:wnext\n'
+
+        f = open('flexi.html','w')
+
+f.write(html)
+
+f.close()
